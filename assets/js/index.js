bf6d0ff27f1d3d6910fa50c8124532483a73b31a
zoya-tech <zoya@fastechsystem.co>
2023-09-25 13:04:35 +0500

    done with task 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,10 @@ function calAvg(arr){
 }
 const arr = [1,2,3,4,5,6,7,8,9]
 console.log(calAvg(arr))
+
+
+// Task 2. Print values of array using map function along with index as following output (”Value at index 1 is 10”, …)
+const arr2 = [9,8,7,6,5,4,3,2,1]
+arr2.map((value, index)=> {
+    console.log(`Value at index ${index} is ${value}`)
+})
